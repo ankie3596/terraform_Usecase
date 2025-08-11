5ad87ad6e8eb8a1e2f7689dca35e8e54b15c56a6
Ankita Mittal <31041708+ankie3596@users.noreply.github.com>
2025-08-11 10:16:39 +0530

    Create variables.tf

---
+++ modules/auto-scaling/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "The name of the Auto Scaling Group"
+  type = string
+}
+
+variable "image_id" {
+  description = "The AMI ID for the instance"
+  type = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type = list(string)
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the Auto Scaling Group"
+  type = list(string)
+}
+
+variable "desired_capacity" {
+  description = "The desired number of instances"
+  type = number
+}
+
+variable "max_size" {
+  description = "The maximum number of instances"
+  type = number
+}
+
+varaible " min_size" {
+  description = "The minimum number of instances"
+  type = number
+}

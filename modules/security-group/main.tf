186821552f1fba2f36bc261bb26a8dc5620b922d
Ankita Mittal <31041708+ankie3596@users.noreply.github.com>
2025-08-11 16:05:04 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "sg" {
   vpc_id = var.vpc_id
 }
 
-\*ingress {
+/*ingress {
   from_port = var.ingress_from_port
   to_port = var.ingress_to_port
   protocol = var.ingress_protocol
